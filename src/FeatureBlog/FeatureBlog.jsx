cbe8a3a9da69069663ac068b89b1809ab138b81d
Arryuann Khanna <aryankhannachd@gmail.com>
2023-11-04 00:16:23 +0530

    second-comm

--- src/FeatureBlog/FeatureBlog.jsx
+++ src/FeatureBlog/FeatureBlog.jsx
@@ -7,6 +7,21 @@ export const FeatureBlog = () => {
         <div className="Feature-blog-catchup">
             LATEST POST
         </div>
+        <div className="Feature-blog-image">
+            
+        </div>
+        <div className="Feature-blog-desc">
+            <div className="Feature-blog-inner-desc">
+            <div className="Feature-blog-time">
+            Mar 23 . 2 min
+            </div>
+            <div className="Feature-blog-title">
+            Back to Fiction: What I'm Reading This Summer
+            </div>
+            <div className="Feature-blog-descr">Create a blog post subtitle that summarizes your post in a few short, punchy sentences and entices your audience to continue reading....
+            </div>
+            </div>
+        </div>
     </div>
   )
 }
